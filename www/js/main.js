07a2388b2a00793546f33afaaf66f3ae91ba7068
luojh-kxpc <1303364524@qq.com>
2015-08-12 17:23:39 +0800

    首页ref 及划动效果

--- www/js/main.js
+++ www/js/main.js
@@ -530,6 +530,7 @@
             init();
     }, false);
 
+
 /*    window.addEventListener('resize', function(){
         musicPlay.init();
     })*/
@@ -541,71 +542,260 @@
         bus_play.put('$ref_dataed', JSON.parse(data));
     });
 
+    var flex_container= document.querySelector('.referrals');
+    var flex_cont= document.querySelector('#ref_content');
+    var flex_container_height= flex_container.offsetHeight;
+    var flex_cont_height= flex_cont.offsetHeight;
+    var Loaded= false;
+    flex_container.addEventListener('scroll', function(ev){
+        var offset= this.scrollTop;
+        flex_cont_height= flex_cont.offsetHeight;
+        if(offset+ flex_container_height+10 >= flex_cont_height){
+            if(!Loaded){
+                Loaded= true;
+                getRef_data();
+            }
+        }
+    }, false);
+
+    function getRef_data(){
+        util.ajax('http://spark.api.xiami.com/api?api_key=263b63d85992a30cc6030aff03c9dfd0&call_id=1439255024287&av=android_101&v=5.0&app_v=5010100&os_v=19_4.4.2&ch=700145&network=1&device_id=353918056359637&platform_id=1&lg=1&utdid=VY0KxJl2HXADAKUwViVKiJ1A&resolution=1280*768&method=music.start&page=4&device_type=Nexus+4&gps=115.17296503%2C30.96320678&ssid=%22KX-WLAN%22&bssid=1c%3A1d%3A86%3Ace%3Aff%3A60&proxy=0&api_sig=a212dfe537e5f36807f14b0e701d7734&access_token=9b80ccb16761a524603f6ec4ad37f5ac', function(data){
+            bus_play.put('$ref_dataed', JSON.parse(data));
+            Loaded= false;
+        });
+    }
+
+
     bus_play.subscribe('$ref_dataed', function(ev, data){
         console.log(ev, data);
+        var ref_cont= document.querySelector('#ref_content');
         //类型模板
         ref_tpl={
-          toplist: function(ele, data){
-              //<li class="item"><div class="album"style="background-image: url(http://pic.xiami.net/images/album/img45/49845/16744797201379914206.jpg@1e_1c_0i_1o_100Q_96w_96h.webp);"></div><div class="song"><div class="song_title">weqr rwrwre ewewer</div><div class="song_author">luojh</div></div><div class="more"><svg class="icon sos-more"><use xlink:href="#sos-more"></use></svg></div></li>
-                var html= '';
+          banner: function(data){
+              var html = '';
+              var ref_banner= document.createElement('div');
+              ref_banner.className= 'ref_banner';
+              html += '<div class="swiper-container"><div class="swiper-wrapper">';
+              for(var s in data.banner){
+                  html += '<div class="swiper-slide" data-title="'+data.banner[s].title+'" data-href="'+data.banner[s].url+'"> <img src="'+data.banner[s].logo+'"> </div>';
+              }
+              html += '</div></div><div class="pagination"></div>';
+              ref_banner.innerHTML= html;
+              ref_cont.appendChild(ref_banner);
+              var banner = new Swiper('.swiper-container',{
+                  pagination: '.pagination',
+                  loop:true,
+                  autoplay: 2500,
+                  grabCursor: true,
+                  paginationClickable: true
+              });
+          },
+          radios: function(data){
+                var html = '';
+                var radios= document.createElement('div');
+                radios.className= 'ref_topLinks';
+                for(var s in data.radios){
+                    var sel= data.radios[s];
+                    html += '<div data-href="'+sel.url+'" class="topLinks_item" style="background-image: url('+sel.logo+');">'+sel.title+'</div>';
+                }
+                radios.innerHTML= html;
+              ref_cont.appendChild(radios);
+          },
+          musicTopList: function( data){  //今日歌曲推介类型
+              var html= '';
+              console.log(333, data.user_name);
+              var topEle= document.createElement('div');
+              topEle.className= 'sq_list';
+
+              if(data.user_name){
+                  html += '<div class="author">'+data.user_name+'</div>';
+              }
+              html += '<div class="title">'+data.title+'</div><ul class="sq_list_cont">';
               for(var s in data.songs){
                 var sel= data.songs[s];
                   html += '<li class="item"><div class="album"style="background-image: url('+sel.album_logo+');"></div><div class="song"><div class="song_title">'+sel.song_name+'</div><div class="song_author">'+sel.singers+'</div></div><div class="more"><svg class="icon sos-more"><use xlink:href="#sos-more"></use></svg></div></li>';
               }
-                ele.innerHTML= html;
-          }
-        };
+              html += '</ul>';
+              topEle.innerHTML= html;
+              ref_cont.appendChild(topEle);
+          },
+            musicTopList_srot: function( data){    //热门歌曲推介类型
+                var html= '';
+                var topEle= document.createElement('div');
+                topEle.className= 'sq_list';
 
-        //首页ref banner
-        var fillFn={
-            ref_banner: function(data){
-                var html = '';
-                var ref_banner= document.querySelector('#ref_banner');
-                for(var s in data.banner){
-                    html += '<div class="swiper-slide" data-title="'+data.banner[s].title+'" data-href="'+data.banner[s].url+'"> <img src="'+data.banner[s].logo+'"> </div>';
+                if(data.user_name){
+                    html += '<div class="author">'+data.user_name+'</div>';
                 }
-                ref_banner.innerHTML= html;
-                var banner = new Swiper('.swiper-container',{
-                    pagination: '.pagination',
-                    loop:true,
-                    autoplay: 2500,
-                    grabCursor: true,
-                    paginationClickable: true
-                });
-            },
-            radios: function(data){
-                var html = '';
-                var radios= document.querySelector('#ref_radios');
-                for(var s in data.radios){
-                    var sel= data.radios[s];
-                    console.log(s,sel)
-                    html += '<div data-href="'+sel.url+'" class="topLinks_item" style="background-image: url('+sel.logo+');">'+sel.title+'</div>';
+                html += '<div class="title">'+data.title+'</div><ul class="sq_list_cont">';
+                for(var s in data.songs){
+                    var sel= data.songs[s];
+                    html += '<li class="item"><div class="album"style="background-image: url('+sel.album_logo+');"></div><div class="song"><div class="song_title">'+sel.song_name+'</div><div class="song_author">'+sel.singers+'</div></div><div class="more"><svg class="icon sos-more"><use xlink:href="#sos-more"></use></svg></div></li>';
                 }
-                radios.innerHTML= html;
+                html += '</ul>';
+                topEle.innerHTML= html;
+                ref_cont.appendChild(topEle);
             },
-            ref_referrals30: function(data){
-                var tagEle= document.querySelector('#ref_referrals30');
-                ref_tpl.toplist(tagEle, data);
+          newMusic: function(data){   //新碟首发
+              var html = '';
+              var newMusicEle= document.createElement('div');
+              newMusicEle.className= 'sq_newMusic';
+              html += '<div class="sq_newMusic_title">'+data.title+'</div><ul class="sq_newMusic_cont">';
+              for(var s in data.collections){
+                var sel = data.collections[s];
+                  html += '<li data-href="'+sel.url+'"><div class="cover" style="background-image:url('+sel.logo+');"></div><div class="title">'+sel.name+'</div><div class="author">'+sel.author+'</div></li>';
+              }
+              html += '</ul>';
+              newMusicEle.innerHTML= html;
+              ref_cont.appendChild(newMusicEle);
+
+          },
+            action: function(data){      //专辑简短推介
+                var action= document.createElement('div');
+                action.className= 'operation';
+                var html;
+                var views=data.views.toString();
+                var views1;
+                if(views && views.length>4){
+                    views1= views.substring(0, views.length-4);
+                    views1= views1+'.'+views.substring(views.length-4, views.length);
+                    views1= parseFloat(views1).toFixed(2);
+                    views= views1+ '万';
+                }
+                html= '<div class="view">'+views+'</div><div class="title">'+data.title+'</div><div class="content">'+data.sub_title+'</div>';
+                action.innerHTML= html;
+                action.style.backgroundImage= 'url('+data.logo+')';
+                action.setAttribute('data-href', data.url);
+                ref_cont.appendChild(action);
+            },
+            mv: function(data){
+                var html= '';
+                var mv= document.createElement('div');
+                mv.className= 'sq_mv';
+                var sel= data.mv;
+                html += '<div class="view">'+sel.play_count+'</div><div class="mask_play"><svg><use xlink:href="#max-player"/></svg></div><div class="itemBody" style="background-image:url('+sel.mv_cover+');"></div><div class="title">'+sel.title+'</div><div class="author">'+sel.artist+'</div>';
+                mv.innerHTML= html;
+                mv.setAttribute('data-href', sel.url);
+                mv.setAttribute('mv_id', sel.mv_id);
+                ref_cont.appendChild(mv);
+            },
+            rec_song: function(data){
+                var html= '';
+                var sel= data.rec_song;
+                var views=sel.play_count.toString();
+                var views1;
+                if(views && views.length>4){
+                    views1= views.substring(0, views.length-4);
+                    views1= views1+'.'+views.substring(views.length-4, views.length);
+                    views1= parseFloat(views1).toFixed(2);
+                    views= views1+ '万';
+                }
+                var rec= document.createElement('div');
+                rec.className= 'sq_rec_song';
+                rec.setAttribute('data-href', sel.url);
+                html += '<div class="view">'+views+'</div><div class="title">'+sel.reason+'</div><div class="cover" style="background-image: url('+sel.album_logo+');"></div><div class="pinyin">'+sel.song_name+'</div><div class="author">'+sel.singers+'</div><div class="more"><svg class="icon sos-more"><use xlink:href="#sos-more"></use></svg></div>';
+                rec.innerHTML= html;
+                ref_cont.appendChild(rec);
+            },
+            featured: function(data){
+                var html= '';
+                var sel= data.collect;
+                var featured= document.createElement('div');
+                featured.className= 'sq_featured';
+                featured.setAttribute('data-href', sel.url);
+                var views=sel.play_count.toString();
+                var views1;
+                if(views && views.length>4){
+                    views1= views.substring(0, views.length-4);
+                    views1= views1+'.'+views.substring(views.length-4, views.length);
+                    views1= parseFloat(views1).toFixed(2);
+                    views= views1+ '万';
+                }
+                html += '<div class="view">'+views+'</div><div class="cover" style="background-image:url('+sel.collect_logo+');"></div><div class="title">'+sel.collect_name+'</div><div class="title_sub">'+sel.author_name+'</div><div class="tag"></div><div class="content">'+sel.description+'</div>';
+                featured.innerHTML= html;
+                ref_cont.appendChild(featured);
+            },
+            rec_song2: function(data){
+                var html= '';
+                var sel= data.album;
+                var views=sel.play_count.toString();
+                var views1;
+                if(views && views.length>4){
+                    views1= views.substring(0, views.length-4);
+                    views1= views1+'.'+views.substring(views.length-4, views.length);
+                    views1= parseFloat(views1).toFixed(2);
+                    views= views1+ '万';
+                }
+                var rec= document.createElement('div');
+                rec.className= 'sq_rec_song2';
+                rec.setAttribute('data-href', sel.source_url);
+                html += '<div class="view">'+views+'</div><div class="title">'+sel.reason+'</div><div class="cover" style="background-image: url('+sel.album_logo+');"></div><div class="pinyin">'+sel.album_name+'</div><div class="author">'+sel.artist_name+'</div><div class="more"></div>';
+                rec.innerHTML= html;
+                ref_cont.appendChild(rec);
+            },
+            featured2: function(data){
+                var html= '';
+                var sel= data.rec_collect;
+                var featured= document.createElement('div');
+                featured.className= 'sq_featured2';
+                featured.setAttribute('data-href', sel.url);
+                var views=sel.play_count.toString();
+                var views1;
+                if(views && views.length>4){
+                    views1= views.substring(0, views.length-4);
+                    views1= views1+'.'+views.substring(views.length-4, views.length);
+                    views1= parseFloat(views1).toFixed(2);
+                    views= views1+ '万';
+                }
+                html += '<div class="view">'+views+'</div><div class="cover" style="background-image:url('+sel.collect_logo+');"></div><div class="title">'+sel.reason+'</div><div class="title_sub">'+sel.collect_name+'</div><div class="author_name">'+sel.author_name+'</div><div class="tag"></div><div class="content">'+sel.description+'</div>';
+                featured.innerHTML= html;
+                ref_cont.appendChild(featured);
             }
         };
+
         //数据填充
         for(var i in data.data.list){
             var colle= data.data.list[i];
-            switch (colle.type)
+            switch (colle.layout)
              {
-                case '1_normal':
-                    fillFn.ref_banner(colle);
+                case 1:
+                    ref_tpl.banner(colle);     //布局类型
                 break;
-                case '20_normal':
-                    fillFn.radios(colle);
+                case 16:
+                    ref_tpl.radios(colle);
                     break;
-                case '19_normal':
-                    fillFn.ref_referrals30(colle);
+                case 15:
+                    ref_tpl.musicTopList(colle);
                     break;
-             }
+                case 10:
+                    ref_tpl.newMusic(colle);
+                    break;
+                case 9:
+                    ref_tpl.musicTopList_srot(colle);
+                    break;
+                case 13:
+                    ref_tpl.action(colle);
+                    break;
+                case 12:
+                    ref_tpl.mv(colle);
+                    break;
+                case 4:
+                    ref_tpl.rec_song(colle);
+                    break;
+                case 7:
+                    ref_tpl.featured(colle);
+                    break;
+                case 8:
+                    ref_tpl.rec_song2(colle);
+                    break;
+                case 6:
+                    ref_tpl.featured2(colle)
+            }
         }
 
-
     });
+
+    smoothness('.sos-show', 1);
+
 }(window.app);
 
--- www/js/util/util.js
+++ www/js/util/util.js
@@ -36,6 +36,70 @@ function toggleClass( elem, c ) {
 }
 
 
+function smoothness(obj,initPage, parentsWidth){
+    if(typeof obj !== 'string') return false;
+    var currentObj= document.querySelector(obj);
+    var parentsW= parentsWidth? parentsWidth: document.documentElement.clientWidth;
+    if(!window['Hammer']){console.log('no Hammer plus!'); return false;}
+    var base= this;
+    var currentObj_ev= new Hammer(currentObj);
+    this.currentPage= initPage? initPage: 0;     //当前页
+    this.leftOffset= -currentPage* parentsW;     //当前偏移
+
+    this.param= {
+        started: 0,
+        currentMovePage: 0,     //当前页
+        initOffset: 0,          //默认偏移
+        leftMoveOffset: 0       //当前偏移
+    };
+    //初始化偏移位置
+    currentObj.style['-webkit-transform']= 'translate3d('+this.leftOffset+'px,0,0) translateZ(0)';
+    currentObj.style['-webkit-transition-timing-function']= 'ease-out';
+
+
+    currentObj_ev.on('panstart', function(ev){
+        currentObj.style['-webkit-transition-duration']= '0s';
+        base.param.currentMovePage= base.currentPage;
+        base.param.initOffset= base.leftOffset;
+        base.param.started= 1;
+    });
+    currentObj_ev.on('panmove', function(ev){
+        base.param.leftMoveOffset= base.param.initOffset+ ev.deltaX;
+        if(base.param.leftMoveOffset>0 || base.param.leftMoveOffset<-parentsW*2){   //是否超出边界
+            base.param.started= -1;
+            return false;
+        }
+        currentObj.style['-webkit-transform']= 'translate3d('+base.param.leftMoveOffset+'px,0,0) translateZ(0)';
+    });
+    currentObj_ev.on('panend', function(ev){
+        if(base.param.started== -1){
+            return false;
+        }
+        base.param.started= 0;
+        //归位
+        if(ev.offsetDirection== 2){
+            if(base.param.leftMoveOffset> base.param.initOffset-(-parentsW/3*2)){
+                base.param.leftMoveOffset= base.param.initOffset+parentsW;
+                base.leftOffset= base.param.leftMoveOffset;
+            }else{
+                base.param.leftMoveOffset= base.param.initOffset;
+            }
+        }else if(ev.offsetDirection == 4){
+            if(base.param.leftMoveOffset< base.param.initOffset-parentsW/3*2){
+                base.param.leftMoveOffset= base.param.initOffset-parentsW;
+                base.leftOffset= base.param.leftMoveOffset;
+            }else{
+                base.param.leftMoveOffset= base.param.initOffset;
+            }
+        }else{
+            base.param.leftMoveOffset= base.param.initOffset;
+        }
+        currentObj.style['-webkit-transition-duration']= '0.3s';
+        currentObj.style['-webkit-transform']= 'translate3d('+base.param.leftMoveOffset+'px,0,0) translateZ(0)';
+    });
+}
+
+
 
 util={
     addClass: addClass,
